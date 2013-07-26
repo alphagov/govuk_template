ef5440b60c34a44e5b4574d74936117a2f8e8991
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-07-26 15:42:17 +0000

    Task to build tarball release.

--- Rakefile
+++ Rakefile
@@ -1,11 +1,22 @@
 require "bundler/gem_tasks"
 
+$:.unshift File.expand_path('../lib', __FILE__)
 $:.unshift File.expand_path('../build_tools', __FILE__)
+require "govuk_template/version"
 
 task :build => :compile
 
+desc "Compile assets from ./source into ./app/assets"
 task :compile do
   require 'compiler/asset_compiler'
   puts "Compiling assets into app/assets"
   Compiler::AssetCompiler.compile
+end
+
+namespace :build do
+  desc "Build govuk_template-#{GovukTemplate::VERSION}.tgz into the pkg directory"
+  task :tar => :compile do
+    require 'packager/tar_packager'
+    Packager::TarPackager.build
+  end
 end
---
+++ build_tools/compiler/template_processor.rb
@@ -0,0 +1,47 @@
+require 'erb'
+
+module Compiler
+  class TemplateProcessor
+
+    def initialize(file)
+      @file = file
+      @is_stylesheet = !! file =~ /\.css\.erb\z/
+    end
+
+    def process
+      render_erb do |section = :layout|
+        handle_yield(section)
+      end
+    end
+
+    def render_erb
+      ERB.new(File.read(@file)).result(binding)
+    end
+
+    def handle_yield(section = :layout)
+      if section == :layout
+        "<!-- Page content goes here -->"
+      end
+    end
+
+    def content_for?(*args)
+      false
+    end
+
+    def asset_path(file, optons={})
+      return file if @is_stylesheet
+      case File.extname(file)
+      when '.css'
+        "/assets/stylesheets/#{file}"
+      when '.js'
+        "/assets/javascripts/#{file}"
+      else
+        "/assets/images/#{file}"
+      end
+    end
+
+    def method_missing(name, *args)
+      puts "#{name} #{args.inspect}"
+    end
+  end
+end
