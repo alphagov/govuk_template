6955cf66a1c8928f896aee9766e292c41196f66e
Edd Sowden <e@e26.co.uk>
2013-09-17 13:49:35 +0000

    Refactor cookie methods for a simpler api
    - Namespaced in the GOVUK namespace
    - Follows jQuery style setter and getters

--- source/assets/javascripts/core.js
+++ source/assets/javascripts/core.js
@@ -1,38 +1,60 @@
-//Reusable functions
-var Alphagov = {
-  daysInMsec: function(d) {
-    return d * 24 * 60 * 60 * 1000;
-  },
-  cookie_domain: function() {
-    var host_parts = document.location.host.split(':')[0].split('.').slice(-3);
-    return '.' + host_parts.join('.');
-  },
-  read_cookie: function(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-      var cookies = document.cookie.split(';');
-      for (var i = 0; i < cookies.length; i++) {
-        var cookie = jQuery.trim(cookies[i]);
-        if (cookie.substring(0, name.length + 1) == (name + '=')) {
-          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-          break;
-        }
-      }
+if (typeof window.GOVUK === 'undefined') {
+  window.GOVUK = {};
+}
+/*
+  Cookie methods
+  ==============
+
+  Usage: 
+
+    Setting a cookie:
+    GOVUK.cookie('hobnob', 'tasty', { days: 30 });
+    
+    Reading a cookie:
+    GOVUK.cookie('hobnob');
+    
+    Deleting a cookie:
+    GOVUK.cookie('hobnob', null);
+*/   
+GOVUK.cookie = function (name, value, options) {
+  if(typeof value !== 'undefined'){
+    if(value === false || value === null) {
+      return GOVUK.setCookie(name, '', { days: -1 });
+    } else {
+      return GOVUK.setCookie(name, value, options);
     }
-    return cookieValue;
-  },
-  delete_cookie: function(name) {
-    if (document.cookie && document.cookie !== '') {
-      var date = new Date();
-      date.setTime(date.getTime() - Alphagov.daysInMsec(1)); // 1 day ago
-      document.cookie = name + "=; expires=" + date.toGMTString() + "; domain=" + Alphagov.cookie_domain() + "; path=/";
-    }
-  },
-  write_cookie: function(name, value) {
-    var date = new Date();
-    date.setTime(date.getTime() + Alphagov.daysInMsec(4 * 30)); // 4 nominal 30-day months in the future
-    document.cookie = name + "=" + encodeURIComponent(value) + "; expires=" + date.toGMTString() + "; domain=" + Alphagov.cookie_domain() + "; path=/";
+  } else {
+    return GOVUK.getCookie(name);
   }
+};
+GOVUK.setCookie: function (name, value, options) {
+  if(typeof options === 'undefined') {
+    options = {};
+  }
+  var cookieString = name + "=" + value + "; path=/";
+  if (options.days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (options.days * 24 * 60 * 60 * 1000));
+    cookieString = cookieString + "; expires=" + date.toGMTString();
+  }
+  if (document.location.protocol == 'https:'){
+    cookieString = cookieString + "; Secure";
+  }
+  document.cookie = cookieString;
+};
+GOVUK.getCookie = function (name) {
+  var nameEQ = name + "=";
+  var cookies = document.cookie.split(';');
+  for(var i = 0, len = cookies.length; i < len; i++) {
+    var cookie = cookies[i];
+    while (cookie.charAt(0) == ' ') {
+      cookie = cookie.substring(1, cookie.length);
+    }
+    if (cookie.indexOf(nameEQ) === 0) {
+      return decodeURIComponent(cookie.substring(nameEQ.length));
+    }
+  }
+  return null;
 };
 
 function recordOutboundLink(e) {
