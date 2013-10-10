173234249fade9b347976ce0cd80c51b5486f50d
Jacob Ashdown <jcbashdown@googlemail.com>
2013-10-10 17:45:49 +0100

    compile and delete app as part of run
    not properly cleaning after play?
    established don't need rules to process erb css or do I? end to end just
    on process?

--- spec/build_tools/packager/underscore_packager_spec.rb
+++ spec/build_tools/packager/underscore_packager_spec.rb
@@ -1,19 +1,23 @@
 require 'spec_helper'
+require File.join(PROJECT_ROOT, 'build_tools/compiler/asset_compiler.rb')
 require File.join(PROJECT_ROOT, 'build_tools/packager/underscore_packager.rb')
 
 describe Packager::UnderscorePackager do
   let(:generated_template_path) {File.join(PROJECT_ROOT, "pkg/underscore_govuk_template-#{GovukTemplate::VERSION}/views/layouts/govuk_template.html")}
   let(:generated_directory_path) {File.join(PROJECT_ROOT, "pkg/underscore_govuk_template-#{GovukTemplate::VERSION}")}
+  let(:app_path) {File.join(PROJECT_ROOT, "app")}
   subject {described_class.new}
 
   after do
     FileUtils.rm_rf(generated_directory_path)
+    FileUtils.rm_rf(app_path)
   end
 
   context "functional" do
     describe "build" do
       let(:example_template_path) {File.join(SPEC_ROOT, 'support/examples/underscore_govuk_template.html')}
       it "should output the correct template" do
+        Compiler::AssetCompiler.compile
         subject.build( without_tarball = true )
         File.read(generated_template_path).should == File.read(example_template_path)
         #should test for all erb
