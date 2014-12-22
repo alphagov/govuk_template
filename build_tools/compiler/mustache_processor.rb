dc26f86cf8d9c655addb9c243c4e46948923370b
James Abley <james.abley@gmail.com>
2014-12-22 10:11:39 +0000

    Remove whitespace from mustache tags’ content
    See https://github.com/alphagov/govuk_template_mustache/issues/4

--- build_tools/compiler/mustache_processor.rb
+++ build_tools/compiler/mustache_processor.rb
@@ -5,21 +5,21 @@ module Compiler
   class MustacheProcessor < TemplateProcessor
 
     @@yield_hash = {
-      after_header: "{{{ afterHeader }}}",
-      body_classes: "{{ bodyClasses }}",
-      body_start: "{{{ bodyStart }}}",
-      body_end: "{{{ bodyEnd }}}",
-      content: "{{{ content }}}",
-      cookie_message: "{{{ cookieMessage }}}",
-      footer_support_links: "{{{ footerSupportLinks }}}",
-      footer_top: "{{{ footerTop }}}",
-      head: "{{{ head }}}",
-      header_class: "{{{ headerClass }}}",
-      html_lang: "{{ htmlLang }}",
-      inside_header: "{{{ insideHeader }}}",
-      page_title: "{{ pageTitle }}",
-      proposition_header: "{{{ propositionHeader }}}",
-      top_of_page: "{{{ topOfPage }}}"
+      after_header: "{{{afterHeader}}}",
+      body_classes: "{{bodyClasses}}",
+      body_start: "{{{bodyStart}}}",
+      body_end: "{{{bodyEnd}}}",
+      content: "{{{content}}}",
+      cookie_message: "{{{cookieMessage}}}",
+      footer_support_links: "{{{footerSupportLinks}}}",
+      footer_top: "{{{footerTop}}}",
+      head: "{{{head}}}",
+      header_class: "{{{headerClass}}}",
+      html_lang: "{{htmlLang}}",
+      inside_header: "{{{insideHeader}}}",
+      page_title: "{{pageTitle}}",
+      proposition_header: "{{{propositionHeader}}}",
+      top_of_page: "{{{topOfPage}}}"
     }
 
     def handle_yield(section = :layout)
@@ -31,11 +31,11 @@ module Compiler
       return "#{file}?#{query_string}" if @is_stylesheet
       case File.extname(file)
       when '.css'
-        "{{{ assetPath }}}stylesheets/#{file}?#{query_string}"
+        "{{{assetPath}}}stylesheets/#{file}?#{query_string}"
       when '.js'
-        "{{{ assetPath }}}javascripts/#{file}?#{query_string}"
+        "{{{assetPath}}}javascripts/#{file}?#{query_string}"
       else
-        "{{{ assetPath }}}images/#{file}?#{query_string}"
+        "{{{assetPath}}}images/#{file}?#{query_string}"
       end
     end
 
