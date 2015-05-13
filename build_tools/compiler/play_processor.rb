7c5458a4ff92b05b3524386c7053a2ff4e1db672
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-05-13 13:32:01 +0000

    Reduce duplication in Compiler::PlayProcessor

--- build_tools/compiler/play_processor.rb
+++ build_tools/compiler/play_processor.rb
@@ -4,6 +4,24 @@ require 'compiler/template_processor'
 module Compiler
   class PlayProcessor < TemplateProcessor
 
+    @@yield_hash = {
+      layout: "<!-- Page content goes here -->",
+      html_lang: "@htmlLang.getOrElse(\"en\")",
+      page_title: "@title.getOrElse(\"GOV.UK - The best place to find government services and information\")",
+      top_of_page: "@(title: Option[String], bodyClasses: Option[String], htmlLang: Option[String] = None)(head:Html, bodyStart:Html, bodyEnd:Html, insideHeader:Html, afterHeader:Html, footerTop:Html, footerLinks:Html, headerClass:Html = Html.empty, propositionHeader:Html = Html.empty)(content:Html)",
+      head: "@head",
+      body_classes: "@bodyClasses.getOrElse(\"\")",
+      header_class: "@headerClass",
+      propositional_header: "@propositionHeader",
+      content: "@content",
+      body_start: "@bodyStart",
+      body_end: "@bodyEnd",
+      inside_header: "@insideHeader",
+      after_header: "@afterHeader",
+      footer_top: "@footerTop",
+      footer_support_links: "@footerLinks",
+    }
+
     def render_erb
       f=File.read(@file)
       f.gsub!(/@-ms-viewport/, "@@-ms-viewport") unless @is_stylesheet
@@ -11,40 +29,7 @@ module Compiler
     end
 
     def handle_yield(section = :layout)
-      case section
-      when :layout
-        "<!-- Page content goes here -->"
-      when :html_lang
-        "@htmlLang.getOrElse(\"en\")"
-      when :page_title
-        "@title.getOrElse(\"GOV.UK - The best place to find government services and information\")"
-      when :top_of_page
-        "@(title: Option[String], bodyClasses: Option[String], htmlLang: Option[String] = None)(head:Html, bodyStart:Html, bodyEnd:Html, insideHeader:Html, afterHeader:Html, footerTop:Html, footerLinks:Html, headerClass:Html = Html.empty, propositionHeader:Html = Html.empty)(content:Html)"
-      when :head
-        "@head"
-      when :body_classes
-        "@bodyClasses.getOrElse(\"\")"
-      when :header_class
-        "@headerClass"
-      when :propositional_header
-        "@propositionHeader"
-      when :content
-        "@content"
-      when :body_start
-        "@bodyStart"
-      when :body_end
-        "@bodyEnd"
-      when :inside_header
-        "@insideHeader"
-      when :after_header
-        "@afterHeader"
-      when :footer_top
-        "@footerTop"
-      when :footer_support_links
-        "@footerLinks"
-      else
-        ""
-      end
+      @@yield_hash[section]
     end
 
     def asset_path(file, options={})
@@ -60,10 +45,7 @@ module Compiler
     end
 
     def content_for?(*args)
-      [:layout, :page_title, :content, :head, :body_classes, :body_start, :body_end,
-        :top_of_page, :inside_header, :after_header, :footer_top,
-        :footer_support_links, :html_lang, :header_class, :propositional_header
-      ].include? args[0]
+      @@yield_hash.has_key?(args[0])
     end
   end
 end
