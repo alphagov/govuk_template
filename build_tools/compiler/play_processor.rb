b694846a4cee88cbbca1d93cc2aa0e93f1c70479
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-05-13 13:39:50 +0000

    Avoid having to escape double quotes
    Trying to make these easier to read.

--- build_tools/compiler/play_processor.rb
+++ build_tools/compiler/play_processor.rb
@@ -5,21 +5,21 @@ module Compiler
   class PlayProcessor < TemplateProcessor
 
     @@yield_hash = {
-      layout: "<!-- Page content goes here -->",
-      html_lang: "@htmlLang.getOrElse(\"en\")",
-      page_title: "@title.getOrElse(\"GOV.UK - The best place to find government services and information\")",
-      top_of_page: "@(title: Option[String], bodyClasses: Option[String], htmlLang: Option[String] = None)(head:Html, bodyStart:Html, bodyEnd:Html, insideHeader:Html, afterHeader:Html, footerTop:Html, footerLinks:Html, headerClass:Html = Html.empty, propositionHeader:Html = Html.empty)(content:Html)",
-      head: "@head",
-      body_classes: "@bodyClasses.getOrElse(\"\")",
-      header_class: "@headerClass",
-      propositional_header: "@propositionHeader",
-      content: "@content",
-      body_start: "@bodyStart",
-      body_end: "@bodyEnd",
-      inside_header: "@insideHeader",
-      after_header: "@afterHeader",
-      footer_top: "@footerTop",
-      footer_support_links: "@footerLinks",
+      layout: '<!-- Page content goes here -->',
+      html_lang: '@htmlLang.getOrElse("en")',
+      page_title: '@title.getOrElse("GOV.UK - The best place to find government services and information")',
+      top_of_page: '@(title: Option[String], bodyClasses: Option[String], htmlLang: Option[String] = None)(head:Html, bodyStart:Html, bodyEnd:Html, insideHeader:Html, afterHeader:Html, footerTop:Html, footerLinks:Html, headerClass:Html = Html.empty, propositionHeader:Html = Html.empty)(content:Html)',
+      head: '@head',
+      body_classes: '@bodyClasses.getOrElse("")',
+      header_class: '@headerClass',
+      propositional_header: '@propositionHeader',
+      content: '@content',
+      body_start: '@bodyStart',
+      body_end: '@bodyEnd',
+      inside_header: '@insideHeader',
+      after_header: '@afterHeader',
+      footer_top: '@footerTop',
+      footer_support_links: '@footerLinks',
     }
 
     def render_erb
@@ -36,11 +36,11 @@ module Compiler
       return file if @is_stylesheet
       case File.extname(file)
       when '.css'
-        "@routes.Template.at(\"stylesheets/#{file}\")"
+        %Q{@routes.Template.at("stylesheets/#{file}")}
       when '.js'
-        "@routes.Template.at(\"javascripts/#{file}\")"
+        %Q{@routes.Template.at("javascripts/#{file}")}
       else
-        "@routes.Template.at(\"images/#{file}\")"
+        %Q{@routes.Template.at("images/#{file}")}
       end
     end
 
