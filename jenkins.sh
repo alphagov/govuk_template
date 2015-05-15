426bcb1bc928c6168de4a90b806a387b7844c29e
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-05-15 17:36:15 +0000

    Add infrastructure for running integration tests

--- jenkins.sh
+++ jenkins.sh
@@ -6,7 +6,7 @@ rm -f Gemfile.lock
 git clean -fdx
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 
-bundle exec rake spec
+bundle exec rake spec integration_tests
 
 # If $PUBLISH_TEMPLATE is set, try and release the template
 if [[ -n "$PUBLISH_TEMPLATE" ]]; then
