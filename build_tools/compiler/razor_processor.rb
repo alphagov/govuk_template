c44184114b1d8c36dba3bd638589d7ae95d2f8fc
daveraine <daveraine@users.noreply.github.com>
2015-06-17 09:56:34 +0100

    Add new sections to razor processor. Update to use Html.Raw.

--- build_tools/compiler/razor_processor.rb
+++ build_tools/compiler/razor_processor.rb
@@ -15,23 +15,23 @@ module Compiler
       when :layout
         "<!-- Page content goes here -->"
       when :html_lang
-        '@(ViewBag.HtmlLang ?? "en")'
+        '@Html.Raw(ViewBag.HtmlLang ?? "en")'
       when :page_title
-        '@(ViewBag.Title ?? "GOV.UK - The best place to find government services and information")'
+        '@Html.Raw(ViewBag.Title ?? "GOV.UK - The best place to find government services and information")'
       when :top_of_page
         render_section_for(:top_of_page)
       when :head
         render_section_for(:head)
       when :body_classes
-        "@(ViewBag.BodyClasses ?? string.Empty)"
+        "@Html.Raw(ViewBag.BodyClasses ?? string.Empty)"
       when :body_start
         render_section_for(:body_start)
       when :cookie_message
-        '@(ViewBag.CookieMessage ?? "<p>GOV.UK uses cookies to make the site simpler. <a href=\"https://www.gov.uk/help/cookies\">Find out more about cookies</a></p>")'
+        '@Html.Raw(ViewBag.CookieMessage ?? "<p>GOV.UK uses cookies to make the site simpler. <a href=\"https://www.gov.uk/help/cookies\">Find out more about cookies</a></p>")'
       when :header_class
-        "@(ViewBag.HeaderClass ?? string.Empty)"
+        "@Html.Raw(ViewBag.HeaderClass ?? string.Empty)"
       when :homepage_url
-        '@(ViewBag.HomepageUrl ?? "https://www.gov.uk/")'
+        '@Html.Raw(ViewBag.HomepageUrl ?? "https://www.gov.uk/")'
       when :proposition_header
         render_section_for(:proposition_header)
       when :content
@@ -46,6 +46,16 @@ module Compiler
         render_section_for(:footer_top)
       when :footer_support_links
         render_section_for(:footer_support_links)
+      when :skip_link_message
+        '@Html.Raw(ViewBag.SkipLinkMessage ?? "Skip to main content")'
+      when :logo_link_title
+        '@Html.Raw(ViewBag.LogoLinkTitle ?? "Go to the GOV.UK homepage")'
+      when :global_header_text
+        '@Html.Raw(ViewBag.GlobalHeaderText ?? "GOV.UK")'
+      when :licence_message
+        '@Html.Raw(ViewBag.LicenceMessage ?? "<p>All content is available under the <a href=\"https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/\" rel=\"license\">Open Government Licence v3.0</a>, except where otherwise stated</p>")'
+      when :crown_copyright_message
+        '@Html.Raw(ViewBag.CrownCopyrightMessage ?? "&copy; Crown copyright")'
       else
         raise "Unexpected section: #{section.inspect}"
       end
