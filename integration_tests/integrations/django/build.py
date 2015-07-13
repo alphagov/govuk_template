e49e84baa64479dd46535363b8b866b85a41b2ee
Fred Marecesche <fred@froddd.co.uk>
2015-07-13 14:36:24 +0100

    Add Django integration test

---
+++ integration_tests/integrations/django/build.py
@@ -0,0 +1,26 @@
+import django
+from django.conf import settings
+from django.template.loader import render_to_string
+
+settings.configure(
+    TEMPLATES=[{
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["vendor/django_govuk_template/views/layouts", "templates"],
+    }],
+    INSTALLED_APPS=["django.contrib.staticfiles"],
+    STATIC_URL="/static/"
+)
+
+django.setup()
+
+context = {
+    "html_lang": "rb",
+    "skip_link_message": "Custom skip link text",
+    "logo_link_title": "Custom logo link title text",
+    "crown_copyright_message": "Custom crown copyright message text",
+}
+
+content = render_to_string("test_template.html", context)
+
+with open("../../html_for_testing/django_integration_test_app.html", "w") as static_file:
+    static_file.write(content)
---
+++ integration_tests/integrations/django/build.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+set -e
+
+rm -rf vendor/django_govuk_template/*
+
+mkdir -p vendor/django_govuk_template
+
+# strip-components lets us decompress into a directory without a version number
+# TODO: Pick the latest, not every matching tgz file!
+tar -zxf ../../../pkg/django_govuk_template-*.tgz -C vendor/django_govuk_template --strip-components 1
+
+pip install virtualenv
+
+virtualenv .env
+source .env/bin/activate
+
+pip install Django
+
+python build.py
