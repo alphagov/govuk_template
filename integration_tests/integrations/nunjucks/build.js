66f73187647d84ad0cf935aeb6094307a7d95e44
Chris Hill-Scott <me@quis.cc>
2016-02-08 14:41:50 +0000

    Add integration test for nunjucks renderer
    This commit adds an integration test for rendering the *Jinja* GOV.UK Template
    with the Nunjucks[1] template rendering engine. The test setup is based on the
    Mustache inheritance one.
    
    The two templating languages are mostly[2] compatible. Having a test will ensure
    that we don’t get caught out by any of the incompatibilities.
    
    This is to avoid having to maintain two separate, but identical versions of the
    template for the two different languages, as discussed in [3].
    
    1. https://mozilla.github.io/nunjucks/
    2. http://mozilla.github.io/nunjucks/faq.html#can-i-use-the-same-templates-between-nunjucks-and-jinja2-what-are-the-differences
    3. https://github.com/alphagov/govuk_template/pull/193

---
+++ integration_tests/integrations/nunjucks/build.js
@@ -0,0 +1,20 @@
+var nunjucks = require('nunjucks'),
+    fs = require('fs'),
+    path = require('path'),
+    env;
+
+env = new nunjucks.Environment(new nunjucks.FileSystemLoader([
+  path.normalize(__dirname + '/templates/'),
+  path.normalize(__dirname + '/vendor/jinja_govuk_template/views/layouts/')
+]));
+
+fs.writeFileSync(
+  path.normalize(__dirname + '/../../html_for_testing/nunchucks_integration_test_app.html'),
+  env.render('test_template.html', {
+      'html_lang': 'rb',
+      'skip_link_message': 'Custom skip link text',
+      'logo_link_title': 'Custom logo link title text',
+      'crown_copyright_message': 'Custom crown copyright message text',
+  }),
+  { encoding : 'utf-8' }
+);
