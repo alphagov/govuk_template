39310d34209a3b25fc9379d398de98d0f0d949ef
Tom Byers <tombaromba@gmail.com>
2017-05-17 20:15:28 +0100

    Hard-code building the gem

--- build_tools/packager/erb_packager.rb
+++ build_tools/packager/erb_packager.rb
@@ -10,11 +10,20 @@ module Packager
 
     def build
       Dir.chdir @repo_root do
-        gem = GemPublisher::Builder.new.build('govuk_template.gemspec')
+        stdout_str = run('gem build govuk_template.gemspec')
+        gem = stdout_str[/File:\s+(.+)/, 1]
 
         @repo_root.join('pkg').mkpath
         FileUtils.mv(gem, "pkg")
       end
     end
+
+    private
+
+    def run(command)
+      output, status = Open3.capture2e(command)
+      abort "Error running #{command}: exit #{status.exitstatus}\n#{output}" if status.exitstatus > 0
+      output
+    end    
   end
 end
