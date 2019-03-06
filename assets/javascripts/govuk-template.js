b94cb325182da8018e538565c258aaab20feb6e9
GOV.UK Design System CI <design-system-pipeline@digital.cabinet-office.gov.uk>
2019-03-06 11:36:16 +0000

    Publishing govuk_template examples version 0.26.0

--- assets/javascripts/govuk-template.js
+++ assets/javascripts/govuk-template.js
@@ -60,20 +60,31 @@
   };
 }).call(this);
 (function () {
-  "use strict"
-  var root = this;
-  if(typeof root.GOVUK === 'undefined') { root.GOVUK = {}; }
+  'use strict'
+  var root = this
+  if (typeof root.GOVUK === 'undefined') { root.GOVUK = {} }
 
   GOVUK.addCookieMessage = function () {
-    var message = document.getElementById('global-cookie-message'),
-        hasCookieMessage = (message && GOVUK.cookie('seen_cookie_message') === null);
+    var message = document.getElementById('global-cookie-message')
+
+    var hasCookieMessage = (message && GOVUK.cookie('seen_cookie_message') === null)
 
     if (hasCookieMessage) {
-      message.style.display = 'block';
-      GOVUK.cookie('seen_cookie_message', 'yes', { days: 28 });
-    }
-  };
-}).call(this);
+      message.style.display = 'block'
+      GOVUK.cookie('seen_cookie_message', 'yes', { days: 28 })
+
+      document.addEventListener('DOMContentLoaded', function (event) {
+        if (GOVUK.analytics && typeof GOVUK.analytics.trackEvent === 'function') {
+          GOVUK.analytics.trackEvent('cookieBanner', 'Cookie banner shown', {
+            value: 1,
+            nonInteraction: true
+          })
+        }
+      })
+    };
+  }
+}).call(this)
+;
 (function() {
   "use strict"
 
